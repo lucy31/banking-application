6f2194dd9cd9ca47ff175f3da60bf7381f1b42ef
= <=>
2021-07-06 01:43:16 +0530

    Adding register method print

---
+++ src/main/java/com/bank/management/controller/BankController.java
@@ -0,0 +1,26 @@
+package com.bank.management.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class BankController {
+
+	@RequestMapping("/login")
+	@ResponseBody
+	public ModelAndView login(String username, String password) {
+		ModelAndView mv = new ModelAndView("");
+		System.out.println("Inside login method");
+		return mv;
+	}
+
+	@RequestMapping("/register")
+	@ResponseBody
+	public ModelAndView register() {
+		ModelAndView mv = new ModelAndView("");
+		System.out.println("Inside register method");
+		return mv;
+	}
+}

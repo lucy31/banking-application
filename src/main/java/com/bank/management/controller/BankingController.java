ff21e9349eed6b9cfd7d4add45f61d9be07f362b
= <=>
2021-07-06 01:15:39 +0530

    creating controller

---
+++ src/main/java/com/bank/management/controller/BankingController.java
@@ -0,0 +1,24 @@
+package com.bank.management.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class BankController {
+
+	@RequestMapping("/login")
+	@ResponseBody
+	public ModelAndView login(String username, String password) {
+		ModelAndView mv = new ModelAndView("");
+		return mv;
+	}
+
+	@RequestMapping("/register")
+	@ResponseBody
+	public ModelAndView register() {
+		ModelAndView mv = new ModelAndView("");
+		return mv;
+	}
+}
